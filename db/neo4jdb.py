9aac1c75712e769b1c94ee643ce9e39baf364e70
Roman <39987014+roman-la@users.noreply.github.com>
2021-09-18 10:51:25 +0200

    Update neo4jdb.py

--- db/neo4jdb.py
+++ db/neo4jdb.py
@@ -2,7 +2,7 @@ from neomodel import DoesNotExist, db, StructuredNode, StringProperty, UniqueIdP
     FloatProperty, StructuredRel
 from datetime import datetime
 
-db.set_connection('bolt://user:pw@localhost:7687')
+db.set_connection('bolt://user:pw@127.0.0.1:7687')
 
 
 def clear_db():
